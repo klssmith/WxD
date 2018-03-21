559c3351843f5694edece452f5987d6b6c17b019
Katie Smith <katie@fastmail.com>
2018-03-21 20:01:31 +0000

    Hook up the DatapointClient to a view

--- app/views.py
+++ app/views.py
@@ -1,8 +1,24 @@
-from flask import render_template
+import os
+
+from flask import abort, render_template
 
 from app import app
+from app.datapoint_client.client import DatapointClient
+from app.datapoint_client.errors import SiteError
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/observations/<int:site_id>')
+def site(site_id):
+    client = DatapointClient(os.getenv('DATAPOINT_API_KEY'))
+
+    try:
+        obs = client.get_all_obs_for_site(site_id)
+    except SiteError:
+        abort(404)
+
+    return render_template('observation_single_site.html', obs=obs)
