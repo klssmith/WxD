e18f53359e93b2b50f1478a31ea1c707c8b3a346
Katie Smith <katie@fastmail.com>
2018-04-29 18:08:16 +0100

    Set up Postgres database
    Added in Flask-Migrate and Flask-SQLAlchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,14 @@
+import os
+
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI', 'postgresql://localhost/wxd')
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from app import views  # noqa
