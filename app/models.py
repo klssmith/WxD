4b4dcfc0b772ce39f3b522926a44a3d0755d6429
Katie Smith <katie@fastmail.com>
2018-04-29 20:00:43 +0100

    Add Site model

---
+++ app/models.py
@@ -0,0 +1,16 @@
+from app import db
+
+
+class Site(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False, index=True)
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
+    elevation = db.Column(db.Float)
+    region = db.Column(db.String(100))
+    unitary_auth_area = db.Column(db.String(100))
+    obs_source = db.Column(db.String(100))
+    national_park = db.Column(db.String(100))
+
+    def __repr__(self):
+        return '<Site: {} {}>'.format(self.id, self.name)
