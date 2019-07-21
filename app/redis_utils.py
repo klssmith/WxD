3ae37f3d3fb74e99eee11c8e0c66f164e7bcdfe0
Katie Smith <katie@fastmail.com>
2019-07-21 22:12:31 +0100

    wip wip

---
+++ app/redis_utils.py
@@ -0,0 +1,26 @@
+import json
+from datetime import datetime
+
+import redis
+
+# cam't put in app/__init__ due to circular import errors
+redis_client = redis.Redis(host='localhost', port=6379, db=0)
+
+
+def store_ob_in_redis(site, data):
+    ttl = calculate_ttl()
+    return redis_client.set(site, json.dumps(data), ex=ttl)
+
+
+def is_site_in_redis(site):
+    breakpoint()
+    return redis_client.exists(site)
+
+
+def get_ob_from_redis(site):
+    return json.loads(redis_client.get(site))
+
+
+def calculate_ttl():
+    current_minute = datetime.now().minute
+    return (60 - current_minute) * 60
